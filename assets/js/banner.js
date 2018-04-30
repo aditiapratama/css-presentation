91377cb55679c8db45d3fd51019c1d21d5ac1c46
Andhika Satya <andhika@mykios.co>
2018-04-30 10:08:45 +0700

    add loader for slow network

--- assets/js/banner.js
+++ assets/js/banner.js
@@ -8,3 +8,20 @@ setTimeout( () => {
     m1.style.backgroundSize = 'contain';
   }
 }, 1000);
+
+// CHECK IF ALL IMAGES ARE LOADED
+
+var imgs = document.images,
+    len = imgs.length,
+    counter = 0;
+[].forEach.call( imgs, function( img ) {
+  img.addEventListener( 'load', incrementCounter, false );
+} );
+function incrementCounter() {
+  counter++;
+  if ( counter === len ) {
+      console.log( 'All images loaded!' );
+      var root = document.getElementById('container');
+      root.classList.remove('preload');
+  }
+}
